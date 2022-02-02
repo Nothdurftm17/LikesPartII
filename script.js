3608326bcec5ba34af8a935a94cbf09fff302d6d
Matthew Nothdurft <nothdurftm17@gmail.com>
2022-02-02 10:36:59 -0500

    Changed left profile and made sure the right var were being used for each function to correct the like counter for each box

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var count = 3;
+var count2 = 3;
+var count3 = 3;
 var countElement = document.querySelector('#count');
 var countElement2 = document.querySelector('#count2');
 var countElement3 = document.querySelector('#count3');
@@ -6,15 +8,15 @@ var countElement3 = document.querySelector('#count3');
 
 function add1(){
     count++
-    countElement.innerText = count
+    countElement.innerText++;
 }
 
 function add2(){
-    count++
-    countElement2.innerText = count
+    count2++
+    countElement2.innerText++;
 }
 
 function add3(){
-    count++
-    countElement3.innerText = count
+    count3++
+    countElement3.innerText++;
 }
